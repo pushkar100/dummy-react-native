179e0aa74abf9357f350f45857f26c0d0cc3a352
pushkar100 <pushkar100@gmail.com>
2023-12-05 20:53:51 +0530

    Adding react-native-navigation plus integration

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, Button} from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
