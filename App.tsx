2a7cd88472c0428d8870e6ddd60644abc40d995a
pushkar100 <pushkar100@gmail.com>
2023-12-05 20:34:22 +0530

    Second commit: Adding all non-igored folders and files

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
